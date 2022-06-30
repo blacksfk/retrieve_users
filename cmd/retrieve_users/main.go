6bb59c69b3b51ea5fdc5242fa24462a950a29134
blacksfk <blacksunflowakuttaspam@hotmail.com>
2022-06-30 15:32:39 +1000

    implemented main func tieing the API together

--- cmd/retrieve_users/main.go
+++ cmd/retrieve_users/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"fmt"
+	api "github.com/blacksfk/retrieve_users/http"
+	"github.com/blacksfk/uf"
+	"log"
+	"net/http"
+)
+
+const (
+	ADDRESS = "localhost:6060"
 )
 
 func main() {
-	fmt.Println("hello world!")
+	sconf := &uf.Config{
+		ErrorLogger:  logStdout,
+		AccessLogger: uf.LogStdout,
+	}
+
+	// create server
+	server := uf.NewServer(sconf)
+
+	// define routes
+	routes(server)
+
+	log.Printf("Starting server on %s\n", ADDRESS)
+
+	// anchors aweigh!
+	log.Fatal(http.ListenAndServe(ADDRESS, server))
+}
+
+// Error logger.
+func logStdout(e error) {
+	log.Println(e)
+}
+
+// Route definitions
+func routes(server *uf.Server) {
+	server.Post("/retrieveUsers", api.RetrieveUsers)
 }
